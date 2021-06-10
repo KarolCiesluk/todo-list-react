978a540169d334cf9a38306292826eaaa74c3742
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-06-10 11:07:48 +0200

    set all done interaction

--- src/App.js
+++ src/App.js
@@ -35,6 +35,14 @@ function App() {
     }));
   };
 
+  const setAllDone = () => {
+    setTasks(
+      tasks => tasks.map(task => ({
+        ...task,
+        done: true,
+      })));
+  };
+
   return (
     <Container>
       <Header title="Lista zadań" />
@@ -58,6 +66,7 @@ function App() {
             tasks={tasks}
             hideDone={hideDone}
             toggleHideDone={toggleHideDone}
+            setAllDone={setAllDone}
           />
         }
       />
