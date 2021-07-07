9f9dc49b637f0b908f6c65f36d699a84a5bbf127
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-07-07 10:47:28 +0200

    added function getInitialTasks()

--- src/App.js
+++ src/App.js
@@ -6,10 +6,17 @@ import Header from "./Header";
 import Container from "./Container";
 import { useEffect, useState } from "react";
 
+const getInitialTasks = () => {
+  const tasksFromLocalStorage = localStorage.getItem("tasks");
+
+  return tasksFromLocalStorage
+    ? JSON.parse(tasksFromLocalStorage)
+    : [];
+}
+
 function App() {
   const [hideDone, setHideDone] = useState(false);
-
-  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || []);
+  const [tasks, setTasks] = useState(getInitialTasks);
 
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
@@ -45,7 +52,7 @@ function App() {
     setTasks(tasks => [
       ...tasks,
       {
-        id: tasks.length? tasks[tasks.length - 1].id + 1 : 1,
+        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
         content,
       }
     ])
