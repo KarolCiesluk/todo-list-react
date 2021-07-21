4357f658338d35720f8fc80a997cc0120f3ec6f4
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-07-21 08:22:10 +0200

    added setAllDone in tasksSlice

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -23,9 +23,14 @@ const tasksSlice = createSlice({
 
             tasks.splice(index, 1);
         },
+        setAllDone: state => {
+            for (const task of state.tasks) {
+                task.done = true;
+            }
+        },
     },
 });
 
-export const { addTask, toggleHideDone, toggleTaskDone, removeTask } = tasksSlice.actions;
+export const { addTask, toggleHideDone, toggleTaskDone, removeTask, setAllDone } = tasksSlice.actions;
 export const selectTasks = state => state.tasks;
 export default tasksSlice.reducer;
