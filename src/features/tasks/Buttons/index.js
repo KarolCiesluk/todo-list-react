20f93e947821d96c9cfb7ae758be30d3fc52f3e4
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-07-21 08:22:58 +0200

    setAllDone from tasksSlice in use in Buttons

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { selectTasks, toggleHideDone } from "../tasksSlice";
+import { selectTasks, toggleHideDone, setAllDone } from "../tasksSlice";
 import { Wrapper, Button } from "./styled";
 
-const Buttons = ({ setAllDone }) => {
+const Buttons = () => {
 const {tasks, hideDone} = useSelector(selectTasks);
 const dispatch = useDispatch();
 
@@ -15,7 +15,7 @@ const dispatch = useDispatch();
             {hideDone ? "Pokaż" : "Ukryj"} ukończone
           </Button>
           <Button
-            onClick={setAllDone}
+            onClick={() => dispatch(setAllDone())}
             doneAll
             disabled={tasks.every(({ done }) => done)}
           >
