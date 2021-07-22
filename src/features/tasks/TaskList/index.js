1d0dfbc55e9564def776e88bfd201715363fd731
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-07-22 12:00:59 +0200

    fixed typo in TaskList

--- src/features/tasks/TaskList/index.js
+++ src/features/tasks/TaskList/index.js
@@ -1,9 +1,9 @@
 import { useSelector, useDispatch } from "react-redux";
-import { selectTasks, toggleTaskDone, removeTask } from "../tasksSlice";
+import { selectTasks, toggleTaskDone, removeTask, selectTasksState } from "../tasksSlice";
 import { List, Item, Content, Button } from "./styled";
 
 const TaskList = () => {
-  const { tasks, hideDone } = useSelector(selectTasks);
+  const { tasks, hideDone } = useSelector(selectTasksState);
   const dispatch = useDispatch();
 
   return (
