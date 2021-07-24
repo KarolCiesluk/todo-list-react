71428d27dd8298f2130d8ecbf1d68cb02be74131
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-07-24 20:10:08 +0200

    updated tasksLocalStorage.js

---
+++ src/features/tasks/tasksLocalStorage.js
@@ -0,0 +1,10 @@
+export const saveTasksInLocalStorage = (tasks) => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+export const getTasksFromLocalStorage = () => {
+    return JSON.parse(localStorage.getItem("tasks")) || [];
+};
+
+
+
