f534c2fda8411997565b5e9bc2c15779ec05b9ee
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-07-24 19:39:21 +0200

    fetchExampleTasksSuccess function* added

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -1,19 +1,27 @@
-import {call, delay, put, takeEvery} from "redux-saga/effects";
+import { all, call, delay, put, takeLatest } from "redux-saga/effects";
 import { getExampleTasks } from "./getExampleTasks";
 import { fetchExampleTasks, setTasks, toggleTasksLoading } from "./tasksSlice";
 
+function* fetchExampleTasksSuccess(exampleTasks) {
+    yield all([
+        put(setTasks(exampleTasks)),
+        put(toggleTasksLoading()),
+    ]);
+};
+
 function* fetchExampleTasksHandler() {
     try {
-        yield put(toggleTasksLoading());
-        yield delay(2000);
+        yield all([
+            put(toggleTasksLoading()),
+            delay(2000),
+        ]);
         const exampleTasks = yield call(getExampleTasks);
-        yield put(setTasks(exampleTasks));
-        yield put(toggleTasksLoading());
+        yield fetchExampleTasksSuccess(exampleTasks);
     } catch (error) {
         yield call(alert, "Coś poszło nie tak!");
     }
 }
 
 export function* watchFetchExampleTasks() {
-    yield takeEvery(fetchExampleTasks.type, fetchExampleTasksHandler);
+    yield takeLatest(fetchExampleTasks.type, fetchExampleTasksHandler);
 };
