6b6ed0c27248999df4c87959e75d56ce6e659e73
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-07-25 22:29:30 +0200

    added routing to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { HashRouter, Link, Route, Switch } from "react-router-dom";
 import Tasks from "./features/tasks/Tasks";
 
-const App = () => <Tasks />;
+const App = () => (
+    <HashRouter>
+        <ul>
+            <li><Link to="/autor">O autorze</Link></li>
+            <li><Link to="/">Zadania</Link></li>
+        </ul>
+        <Switch>
+            <Route path="/autor"><div>lalal</div></Route>
+            <Route path="/"><Tasks /></Route>
+        </Switch>
+    </HashRouter>
+);
 
 export default App;
