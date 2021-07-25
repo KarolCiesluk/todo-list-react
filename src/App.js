4ecf87112cfefb4676450f3dd2391c2f85b20a1b
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-07-25 22:38:01 +0200

    added Author component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { HashRouter, Link, Route, Switch } from "react-router-dom";
+import Author from "./features/author";
 import Tasks from "./features/tasks/Tasks";
 
 const App = () => (
@@ -8,7 +9,7 @@ const App = () => (
             <li><Link to="/">Zadania</Link></li>
         </ul>
         <Switch>
-            <Route path="/autor"><div>lalal</div></Route>
+            <Route path="/autor"><Author /></Route>
             <Route path="/"><Tasks /></Route>
         </Switch>
     </HashRouter>
