42469997f383fe46dd48f7ee00425c9daae5d861
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-07-26 10:26:23 +0200

    App.js formatting

--- src/App.js
+++ src/App.js
@@ -7,14 +7,26 @@ import { Navigation, NavigationButton } from "./styled";
 const App = () => (
     <HashRouter>
         <Navigation>
-            <li><NavigationButton to="/autor">O autorze</NavigationButton></li>
-            <li><NavigationButton to="/zadania">Zadania</NavigationButton></li>
+            <li>
+                <NavigationButton to="/autor">O autorze</NavigationButton>
+            </li>
+            <li>
+                <NavigationButton to="/zadania">Zadania</NavigationButton>
+            </li>
         </Navigation>
         <Switch>
-            <Route path="/zadania/:id">lalala</Route>
-            <Route path="/zadania"><Tasks /></Route>
-            <Route path="/autor"><AuthorPage /></Route>
-            <Route path="/"><Redirect to="/zadania" /></Route>
+            <Route path="/zadania/:id">
+                lalala
+            </Route>
+            <Route path="/zadania">
+                <Tasks />
+            </Route>
+            <Route path="/autor">
+                <AuthorPage />
+            </Route>
+            <Route path="/">
+                <Redirect to="/zadania" />
+            </Route>
         </Switch>
     </HashRouter>
 );
