44267c0d6374798b98b23e90bcb77bef9f19b4e5
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-07-26 09:39:45 +0200

    added Author component

---
+++ src/features/authorPage/index.js
@@ -0,0 +1,17 @@
+import Container from "../../common/Container";
+import Header from "../../common/Header";
+import Section from "../../common/Section";
+
+
+const Author = () => (
+    <Container>
+        <Header title="O autorze" />
+        <Section
+            title="Karol Cieśluk"
+            body="lalala"
+        />
+    </Container>
+);
+
+export default Author;
+
