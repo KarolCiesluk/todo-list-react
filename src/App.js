7bb2722b99be6e9dd8bf0c67980f091c80dc8936
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-07-26 09:37:40 +0200

    added styled navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import { HashRouter, Link, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Author from "./features/author";
 import Tasks from "./features/tasks/Tasks";
+import { Navigation, NavigationButton } from "./styled";
 
 const App = () => (
     <HashRouter>
-        <ul>
-            <li><Link to="/autor">O autorze</Link></li>
-            <li><Link to="/">Zadania</Link></li>
-        </ul>
+        <Navigation>
+            <li><NavigationButton to="/autor">O autorze</NavigationButton></li>
+            <li><NavigationButton to="/zadania">Zadania</NavigationButton></li>
+        </Navigation>
         <Switch>
             <Route path="/autor"><Author /></Route>
-            <Route path="/"><Tasks /></Route>
+            <Route path="/zadania"><Tasks /></Route>
         </Switch>
     </HashRouter>
 );
