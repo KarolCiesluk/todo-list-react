80298d2d7d0d9c86388c456211f7f1ba782fa76d
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-08-14 12:54:49 +0200

    moved useQueryParameter and useReplaceQueryParameter to queryParameters.js

--- src/features/tasks/TasksPage/queryParameters.js
+++ src/features/tasks/TasksPage/queryParameters.js
@@ -1,11 +1,18 @@
 import { useHistory, useLocation } from "react-router-dom";
 
+export const useQueryParameter = (searchQueryParamName) => {
+    const location = useLocation();
+
+    return (new URLSearchParams(location.search)).get(searchQueryParamName);
+};
+
 export const useReplaceQueryParameter = () => {
     const location = useLocation();
     const history = useHistory();
-    const searchParams = new URLSearchParams(location.search);
 
     return ({ key, value }) => {
+        const searchParams = new URLSearchParams(location.search);
+        
         if (!value) {
             searchParams.delete(key);
         } else {
